3b906d8c3e77cb9af2bf7eafb63a87bbea200817
jtran <jtran@tulane.edu>
2022-06-26 21:36:20 -0500

    new logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './images/full_logo.svg';
+import logo from './images/full logo.svg';
 import './App.css';
 
 function App() {
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Coming Soon &#8482;</p>
       </header>
     </div>
   );
